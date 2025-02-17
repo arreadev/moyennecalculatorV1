aaf675c4de74b49a1f6dad8cdc78bf08308f5bb1
ArreaDev <vodi.contact@gmail.com>
2025-02-17 14:30:49 +0100

    Create moyenne1.py

---
+++ src/moyenne1.py
@@ -0,0 +1,18 @@
+
+## Calculez la moyenne de 10 notes ayant le même coefficient.
+
+note1 = float(input("Entrez la note 1: "))
+note2 = float(input("Entrez la note 2: "))
+note3 = float(input("Entrez la note 3: "))
+note4 = float(input("Entrez la note 4: "))
+note5 = float(input("Entrez la note 5: "))
+note6 = float(input("Entrez la note 6: "))
+note7 = float(input("Entrez la note 7: "))
+note8 = float(input("Entrez la note 8: "))
+note9 = float(input("Entrez la note 9: "))
+note10 = float(input("Entrez la note 10: "))
+
+moyenne = (note1 + note2 + note3 + note4 + note5 + note6 + note7 + note8 + note9 + note10) / 10
+
+print("La moyenne est de " + str(moyenne))
+input('Vous pouvez quitter le programme...')
